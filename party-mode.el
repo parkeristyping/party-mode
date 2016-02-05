af077027e813b676b928d8eb63ff665695e3e89b
Parker Lawrence <parker.alford@gmail.com>
2016-02-05 13:56:05 -0600

    add some comments to see if it helps build from recipe

--- party-mode.el
+++ party-mode.el
@@ -1,3 +1,37 @@
+;;; party-mode.el --- Ain't no party like an Emacs party -*- lexical-binding: t -*-
+
+;; Copyright © 2014 parkeristyping <parker.alford@gmail.com> 
+
+;; Author: 
+;; URL: https://github.com/parkeristyping/party-mode.el
+;; Keywords: party, music, emms
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "24") (emms "3.0") (async "1.6"))
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Usage:
+
+;; M-x party-mode 
+;; M-x stop-partying
+
+;;; Code:
+
 (setq party-mode-engaged-flag nil)
 (setq party-mode-music-path (concat (file-name-directory #$) "music/crapface.mp3"))
 
@@ -41,3 +75,4 @@
   (emms-stop))
 
 (provide 'party-mode)
+;;; party-mode.el ends here
